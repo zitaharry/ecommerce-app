7715f6b9fa57f62f72815871d120f30f3c67ef82
zitaharry <harrymuigua@gmail.com>
2025-08-15 12:45:19 +0300

    Implement Live Feature

--- sanity/lib/live.ts
+++ sanity/lib/live.ts
@@ -4,10 +4,21 @@
 import { defineLive } from "next-sanity";
 import { client } from './client'
 
-export const { sanityFetch, SanityLive } = defineLive({ 
-  client: client.withConfig({ 
+// Set up viewer token
+const token = process.env.SANITY_API_READ_TOKEN;
+if (!token) {
+  throw new Error("Missing SANITY_API_READ_TOKEN");
+}
+
+export const { sanityFetch, SanityLive } = defineLive({
+  client: client.withConfig({
     // Live content is currently only available on the experimental API
     // https://www.sanity.io/docs/api-versioning
-    apiVersion: 'vX' 
-  }) 
+    apiVersion: 'vX'
+  }),
+  serverToken: token,
+  browserToken: token,
+  fetchOptions: {
+    revalidate: 0
+  }
 });
